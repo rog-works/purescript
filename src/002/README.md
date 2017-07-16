ce88f3e100df7205ea46456f82b357c297de1a31
rog-works <rog-works@yahoo.co.jp>
2017-07-16 16:48:10 +0900

    added: math module

--- src/002/README.md
+++ src/002/README.md
@@ -4,4 +4,5 @@ Getting started 002
 ```
 $ pulp run
 $ pulp build -O --to output.js
+$ bower install purescript-math --save
 ```
--- src/002/src/Main.purs
+++ src/002/src/Main.purs
@@ -1,5 +1,8 @@
 module Main where
 
+import Prelude
+import Math (sqrt)
 import Control.Monad.Eff.Console
 
-main = log "Hello World!"
+diagonal w h = sqrt (w * w + h * h)
+main = logShow (diagonal 3.0 4.0)
